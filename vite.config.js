3ca9aa37b4d7d40ee078ad65f7259f7e8a777b9d
ccc94633-star <ccc94633@gmail.com>
2026-06-08 22:40:54 +0800

    修改vite

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: '/portfolio-chiikawa-generator-web/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
